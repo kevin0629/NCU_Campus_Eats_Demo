b2d838717bc8aedfb29d1fdd3e5b75b380550005
Kevin Kuo <k113423027@g.ncu.edu.tw>
2024-12-16 09:32:29 +0800

    vm ip change

--- app/config.py
+++ app/config.py
@@ -15,7 +15,7 @@ class Config:
     TOKEN_URL = 'https://portal.ncu.edu.tw/oauth2/token'
     USER_INFO_URL = 'https://portal.ncu.edu.tw/apis/oauth/v1/info'
     # REDIRECT_URI = 'http://localhost:5000/customers/callback'  # 回調 URL
-    REDIRECT_URI = 'http://13.208.214.9:5000/customers/callback'  # AWS
+    REDIRECT_URI = 'http://13.208.174.59:5000/customers/callback'  # AWS
     SCOPE = 'id identifier chinese-name email mobile-phone personal-id'
 
     # Flask-Mail 設定

9c57a5c2e73b5e7c3e1fe30f472acac9e3ceb55b
DESKTOP-U8KC2K8\giyak <giyakw4327@gmail.com>
2024-12-19 02:29:57 +0800

    change to localhost connection

--- app/config.py
+++ app/config.py
@@ -2,20 +2,20 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/campus_eats'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:113423027@mysql-1.cfg8ygkqmlab.ap-northeast-3.rds.amazonaws.com/campus_eats' #AWS
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/campus_eats'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:113423027@mysql-1.cfg8ygkqmlab.ap-northeast-3.rds.amazonaws.com/campus_eats' #AWS
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # OAuth 設定
-    # CLIENT_ID = '20241007203637hgWIOoOg6QGH'  # 從中央大學 Portal 申請
-    # CLIENT_SECRET = 'YUustASvU0LWPSFXygagued9EILygcfv4h3xofCYJYAuQEoMXrLatvFy'  # 從中央大學 Portal 申請
-    CLIENT_ID = '20241006124146JNAtIrxu5pib' #AWS
-    CLIENT_SECRET = '6aEUaGj20UIynYA3qp6ezElULErCSuRMYQnseXzqUluoK3NMYT5QfxNk'  #AWS
+    CLIENT_ID = '20241007203637hgWIOoOg6QGH'  # 從中央大學 Portal 申請
+    CLIENT_SECRET = 'YUustASvU0LWPSFXygagued9EILygcfv4h3xofCYJYAuQEoMXrLatvFy'  # 從中央大學 Portal 申請
+    # CLIENT_ID = '20241006124146JNAtIrxu5pib' #AWS
+    # CLIENT_SECRET = '6aEUaGj20UIynYA3qp6ezElULErCSuRMYQnseXzqUluoK3NMYT5QfxNk'  #AWS
     AUTHORIZATION_URL = 'https://portal.ncu.edu.tw/oauth2/authorization'
     TOKEN_URL = 'https://portal.ncu.edu.tw/oauth2/token'
     USER_INFO_URL = 'https://portal.ncu.edu.tw/apis/oauth/v1/info'
-    # REDIRECT_URI = 'http://localhost:5000/customers/callback'  # 回調 URL
-    REDIRECT_URI = 'http://15.152.37.157:5000/customers/callback'  # AWS
+    REDIRECT_URI = 'http://localhost:5000/customers/callback'  # 回調 URL
+    # REDIRECT_URI = 'http://15.152.37.157:5000/customers/callback'  # AWS
     SCOPE = 'id identifier chinese-name email mobile-phone personal-id'
 
     # Flask-Mail 設定
--- run.py
+++ run.py
@@ -11,5 +11,5 @@ app = create_app()
 mail = Mail(app)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True) #aws
-    # app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True) #aws
+    app.run(debug=True)
